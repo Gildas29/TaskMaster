fd6b114000c3809e88051feb8b8853c600aea71b
Gildas29 <gildas.couillard@viacesi.fr>
2025-06-05 09:51:30 +0200

    Ajout local storage

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const form = document.querySelector("form");
 
+// Stocker la liste dans le local storage
+function storeList() {
+  window.localStorage.taskmaster = list.innerHTML;
+}
+
+// Récupère les données du localStorage si il y en a
+function getTask() {
+  if (window.localStorage.taskmaster) {
+    list.innerHTML = window.localStorage.taskmaster;
+  } else {
+    list.innerHTML = `<li>Cliquez sur une tâche pour la supprimer</li>`;
+  }
+}
+
+// Exécute la fonction getTask après chargement de la page
+window.addEventListener("load", getTask);
+
 // Ajouter un élément
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  e.preventDefault(); // Empêche le rechargement de la page
   list.innerHTML += `<li>${item.value}</li>`;
   item.value = "";
+  storeList(); // Met à jour le localStorage
 });
 
 // Supprimer un élément
@@ -14,4 +32,5 @@ list.addEventListener("click", (e) => {
   } else {
     e.target.classList.add("checked");
   }
+  storeList(); // Met à jour le localStorage
 });
